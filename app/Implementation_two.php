190a2efd2eca65430b5fc899cc14c24a4e9e5a4b
Tracy <tracyamara07@gmail.com>
2020-02-05 10:38:15 +0100

    Update Implementation_two.php

--- app/Implementation_two.php
+++ app/Implementation_two.php
@@ -12,15 +12,15 @@ class Teachers
     
     public function borrowBook($requestedBook,$role){
         $jsonData = file_get_contents('books.json');
-        $requestedBook= json_decode($jsonData,true);
+        $bookAvailable= json_decode($jsonData,true);
         if (is_numeric($requestedBook && $role)) {
             throw new Exception('Numeric priority value not expected');
         }
         if(strtolower($role) == "Teacher"){
-            if(in_array($requestedBook,$this->books)){
-                for($i=0; $i < count($this->books); $i++){
-                    if($this->books[$i] == $requestedBook){
-                        unset($this->books[$i]);
+            if(in_array($requestedBook,$this->bookAvailable)){
+                for($i=0; $i < count($this->bookAvailable); $i++){
+                    if($this->bookAvailable[$i] == $requestedBook){
+                        unset($this->bookAvailable[$i]);
                     }
                 }
                 $response="Have your Book";
